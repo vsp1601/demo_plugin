54a510534dd770a55f8cea4345fad0d78e3fe0b5
vsp1601 <prasathkanishk407@gmail.com>
2024-09-06 10:52:27 +0530

    first commit

---
+++ includes/class-shortcode-activator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin activation
+ *
+ * @link       https://www.vsp.com
+ * @since      1.0.0
+ *
+ * @package    Shortcode
+ * @subpackage Shortcode/includes
+ */
+
+/**
+ * Fired during plugin activation.
+ *
+ * This class defines all code necessary to run during the plugin's activation.
+ *
+ * @since      1.0.0
+ * @package    Shortcode
+ * @subpackage Shortcode/includes
+ * @author     VSP <VSP@gmail.com>
+ */
+class Shortcode_Activator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function activate() {
+
+	}
+
+}
---
+++ includes/class-shortcode-deactivator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin deactivation
+ *
+ * @link       https://www.vsp.com
+ * @since      1.0.0
+ *
+ * @package    Shortcode
+ * @subpackage Shortcode/includes
+ */
+
+/**
+ * Fired during plugin deactivation.
+ *
+ * This class defines all code necessary to run during the plugin's deactivation.
+ *
+ * @since      1.0.0
+ * @package    Shortcode
+ * @subpackage Shortcode/includes
+ * @author     VSP <VSP@gmail.com>
+ */
+class Shortcode_Deactivator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function deactivate() {
+
+	}
+
+}
